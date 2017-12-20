5f913c989403a120643b5639708a7c77bcd4bb42
Kyle Nally <kylegnally@gmail.com>
2017-12-20 14:26:42 -0500

    Add mouseclick to scroll map debug function. Add Box2D debug collision boundaries.

--- core/src/com/kylenally/mariobros/Screens/PlayScreen.java
+++ core/src/com/kylenally/mariobros/Screens/PlayScreen.java
@@ -1,14 +1,23 @@
 package com.kylenally.mariobros.Screens;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.kylenally.mariobros.MarioBros;
 import com.kylenally.mariobros.Scenes.Hud;
@@ -23,13 +32,98 @@ public class PlayScreen implements Screen {
     private OrthographicCamera gameCam;
     private Viewport gamePort;
     private Hud hud;
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+    private World world;
+    private Box2DDebugRenderer b2dr;
 
     public PlayScreen(MarioBros game) {
 
         this.game = game;
+
+        // create a camera
         gameCam = new OrthographicCamera();
+
+        // create a viewport and fit the camera to the viewport height and width
         gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gameCam);
+
+        // create a new HUD
         hud =  new Hud(game.batch);
+
+        // instantiate a map loader object
+        mapLoader = new TmxMapLoader();
+
+        // pass the map loader our level tileset
+        map = mapLoader.load("level1.tmx");
+
+        // render the map with an instance of the renderer, passing it
+        // our map
+        renderer = new OrthogonalTiledMapRenderer(map);
+
+        // center the camera on the viewport at z height 0
+        gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+        // create the world
+        world = new World(new Vector2(0, 0), true);
+        b2dr = new Box2DDebugRenderer();
+
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
+        // create the ground bodies/fixtures
+        for (MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        // create the pipe bodies/fixtures
+        for (MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        // create the brick bodies/fixtures
+        for (MapObject object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        // create the coin bodies/fixtures
+        for (MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        // apply the gameport
+        gamePort.apply();
 
     }
 
@@ -41,13 +135,48 @@ public class PlayScreen implements Screen {
     @Override
     public void render(float delta) {
 
+        // call the update method and pass it the delta
+        update(delta);
+
+        // set the view of the renderer to the camera
+        renderer.setView(gameCam);
+
         // color and alpha
         Gdx.gl.glClearColor(0, 0, 0, 1);
 
         // clear the screen
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // render the map
+        renderer.render();
+
+        // render the box2d lines
+        b2dr.render(world, gameCam.combined);
+
+        // project the hud and the stage through the camera and combine them
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+
+        // draw the HUD
         hud.stage.draw();
+
+
+    }
+
+    public void handleInput(float dt) {
+
+        // if an input is touched, move the camera by 100 units multiplied
+        // by the delta value
+        if (Gdx.input.isTouched()) {
+            gameCam.position.x += 100 * dt;
+        }
+        gameCam.update();
+
+
+    }
+
+    public void update(float dt) {
+
+        handleInput(dt);
 
     }
 
